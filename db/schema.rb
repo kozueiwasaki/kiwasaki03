eebece0e961c3211359013708b26026bf43a34d8
kocchan <59197715+kocchan@users.noreply.github.com>
2020-04-06 23:15:35 +0900

    add column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_05_114906) do
+ActiveRecord::Schema.define(version: 2020_04_06_130345) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
