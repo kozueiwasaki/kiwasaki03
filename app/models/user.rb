c99ad3a64490f8946f01cb1faeedec6e7d6844d4
kocchan <59197715+kocchan@users.noreply.github.com>
2020-04-18 23:08:02 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :password, presence: true, length: { in: 6..12 }, on: :create
-  validates :password, length: { in: 6..12 }, on: :update
+  validates :password, presence: true, on: :create
+  validates :password, length: { in: 6..12 }, if: Proc.new { |user| user.password.present? }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   has_many :posts
